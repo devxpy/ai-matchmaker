c7d3263f56455618cefb76721c3797d92435b526
Dev Aggarwal <devxpy@gmail.com>
2023-05-25 19:20:26 +0530

    use redis cache for realtime updates

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+import hashlib
 import io
 import os
+import pickle
 import re
-from functools import lru_cache
+import typing
+from functools import lru_cache, wraps
 from threading import Thread
 from time import sleep
 
@@ -10,6 +13,7 @@ import numpy as np
 import openai
 import pandas as pd
 import plotly.graph_objects as go
+import redis as redis
 import umap
 from dash import dcc, html, dash_table
 from dash.dependencies import Input, Output
@@ -19,7 +23,6 @@ from googleapiclient import discovery
 from googleapiclient.http import MediaIoBaseDownload
 from plotly.subplots import make_subplots
 from sklearn.preprocessing import StandardScaler
-
 
 service_account_key_path = "serviceAccountKey.json"
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = service_account_key_path
@@ -34,6 +37,9 @@ else:
 
 
 SHEETS_URL = config("SHEETS_URL")
+openai.api_key = config("OPENAI_API_KEY")
+r = redis.Redis.from_url(config("REDIS_CACHE_URL", "redis://localhost:6379"))
+
 batch_size = 100
 
 
@@ -61,7 +67,7 @@ app.layout = html.Div(
             # html.Div(id="live-update-text"),
             dcc.Interval(
                 id="interval-component",
-                interval=250,  # in milliseconds
+                interval=1000,  # in milliseconds
                 n_intervals=0,
             ),
         ],
@@ -78,54 +84,61 @@ def get_shared():
 
 
 def bg_thread(shared):
+    prev_bytes = None
     while True:
-        df = download_sheet(furl(SHEETS_URL))
-        keys = df.name.dropna().tolist()
-        docs = df.interests.dropna().tolist()
-
-        # insert data into the db & build the index in batches
-        embeds = [
-            embed
-            for i in range(0, len(docs), batch_size)
-            for embed in get_embeddings(docs[i : i + batch_size])
-        ]
-        embeds = np.array(embeds)
-
-        data = umap.UMAP(random_state=42, n_neighbors=len(keys) // 2).fit_transform(
-            embeds
-        )
-        data = StandardScaler().fit_transform(data)
-
-        shared["plot"] = dict(
-            x=[row[0] for row in data],
-            y=[row[1] for row in data],
-            text=keys,
-        )
-
-        sims = embeds @ embeds.T
-        sorted_sims = np.dstack(
-            np.unravel_index(np.argsort(sims, axis=None), sims.shape)
-        )[0][::-1]
-        visited = set()
-        matches = []
-        for x, y in sorted_sims:
-            if x == y:
-                continue
-            if x in visited or y in visited:
-                continue
-            visited.add(x)
-            visited.add(y)
-            matches.append(
-                {
-                    "Name 1": keys[x],
-                    "Name 2": keys[y],
-                    "Similarity": round(sims[x, y], 4),
-                }
-            )
-
-        shared["top_matches"] = pd.DataFrame.from_records(matches)
-
+        csv_bytes = download_sheet(furl(SHEETS_URL))
+        if prev_bytes != csv_bytes:
+            prev_bytes = csv_bytes
+            df = pd.read_csv(io.BytesIO(csv_bytes))
+            _on_change(df, shared)
         sleep(1)
+
+
+def _on_change(df, shared):
+    df = df.rename(
+        columns=lambda col: col.strip().lower().replace(" ", "_")
+    )
+
+    keys = df.name.dropna().tolist()
+    docs = df.interests.dropna().tolist()
+
+    embeds = np.array([
+        embed
+        for i in range(0, len(docs), batch_size)
+        for embed in get_embeddings(docs[i: i + batch_size])
+    ])
+
+    data = umap.UMAP(random_state=42, n_neighbors=len(keys) // 2).fit_transform(
+        embeds
+    )
+    data = StandardScaler().fit_transform(data)
+    shared["plot"] = dict(
+        x=[row[0] for row in data],
+        y=[row[1] for row in data],
+        text=keys,
+    )
+
+    sims = embeds @ embeds.T
+    sorted_sims = np.dstack(
+        np.unravel_index(np.argsort(sims, axis=None), sims.shape)
+    )[0][::-1]
+    visited = set()
+    matches = []
+    for x, y in sorted_sims:
+        if x == y:
+            continue
+        if x in visited or y in visited:
+            continue
+        visited.add(x)
+        visited.add(y)
+        matches.append(
+            {
+                "Name 1": keys[x],
+                "Name 2": keys[y],
+                "Similarity": round(sims[x, y], 4),
+            }
+        )
+    shared["top_matches"] = pd.DataFrame.from_records(matches)
 
 
 whitespace_re = re.compile("\s+")
@@ -137,12 +150,9 @@ def get_embeddings(texts: list[str], engine: str = "text-embedding-ada-002"):
     return [record["embedding"] for record in res["data"]]
 
 
-@lru_cache
-def _openai_embedding_create(*args, **kwargs):
-    return openai.Embedding.create(*args, **kwargs)
 
 
-def download_sheet(f: furl, mime_type: str = "text/csv") -> pd.DataFrame:
+def download_sheet(f: furl, mime_type: str = "text/csv") -> bytes:
     # get drive file id
     file_id = url_to_gdrive_file_id(f)
     # get metadata
@@ -156,9 +166,7 @@ def download_sheet(f: furl, mime_type: str = "text/csv") -> pd.DataFrame:
     while not done:
         _, done = downloader.next_chunk()
     # to dataframe
-    return pd.read_csv(io.BytesIO(file.getvalue())).rename(
-        columns=lambda x: x.strip().lower().replace(" ", "_")
-    )
+    return file.getvalue()
 
 
 def url_to_gdrive_file_id(f: furl) -> str:
@@ -177,7 +185,7 @@ def url_to_gdrive_file_id(f: furl) -> str:
 
 
 @app.callback(
-    Output("live-update-graph", "figure"), Input("interval-component", "n_intervals")
+    Output("live-update-graph", "figure"), Input("interval-component", "n_intervals"),
 )
 def update_graph_live(n):
     fig = make_subplots()
@@ -203,6 +211,33 @@ def update_matches(n):
         return {}
     df = data["top_matches"]
     return df.to_dict("records")
+
+
+F = typing.TypeVar("F", bound=typing.Callable[..., typing.Any])
+def redis_cache_decorator(fn: F) -> F:
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        # hash the args and kwargs so they are not too long
+        args_hash = hashlib.sha256(f"{args}{kwargs}".encode()).hexdigest()
+        # create a readable cache key
+        cache_key = f"ai-matchmaker/redis-cache-decorator/v1/{fn.__name__}/{args_hash}"
+        cache_val = r.get(cache_key)
+        # if the cache exists, return it
+        if cache_val:
+            return pickle.loads(cache_val)
+        # otherwise, run the function and cache the result
+        else:
+            result = fn(*args, **kwargs)
+            cache_val = pickle.dumps(result)
+            r.set(cache_key, cache_val)
+            return result
+
+    return wrapper
+
+
+@redis_cache_decorator
+def _openai_embedding_create(*args, **kwargs):
+    return openai.Embedding.create(*args, **kwargs)
 
 
 # @app.callback(
