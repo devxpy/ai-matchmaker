4ad9e6c13576a801eed6a5e5172ef7b56b7cbc5e
Dev Aggarwal <devxpy@gmail.com>
2023-05-27 09:22:57 +0530

    better visualizations use sentence embeds

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import io
 import os
 import pickle
 import re
+import string
+import textwrap
 import typing
 from functools import lru_cache, wraps
 from threading import Thread
@@ -10,20 +12,20 @@ from time import sleep
 from traceback import print_exc
 
 import dash
+import networkx as nx
 import numpy as np
 import openai
 import pandas as pd
 import plotly.graph_objects as go
 import redis as redis
-import umap
 from dash import dcc, html, dash_table
 from dash.dependencies import Input, Output
 from decouple import config, UndefinedValueError
 from furl import furl
 from googleapiclient import discovery
 from googleapiclient.http import MediaIoBaseDownload
-from plotly.subplots import make_subplots
-from sklearn.preprocessing import StandardScaler
+from sentence_transformers import SentenceTransformer
+
 
 service_account_key_path = "serviceAccountKey.json"
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = service_account_key_path
@@ -43,35 +45,8 @@ r = redis.Redis.from_url(config("REDIS_CACHE_URL", "redis://localhost:6379"))
 
 batch_size = 100
 
-
 app = dash.Dash(__name__)
 server = app.server
-app.layout = html.Div(
-    html.Div(
-        [
-            html.H1("Top Matches"),
-            dash_table.DataTable(
-                id="live-update-matches",
-                fill_width=False,
-                style_header={
-                    "font-weight": "bold",
-                },
-                style_cell={
-                    "padding": "5px",
-                },
-            ),
-            html.H1("UMAP visualization"),
-            dcc.Graph(id="live-update-graph", style={"height": "80vh"}),
-            # html.Div(id="live-update-text"),
-            dcc.Interval(
-                id="interval-component",
-                interval=1000,  # in milliseconds
-                n_intervals=0,
-            ),
-        ],
-        style={"fontFamily": "system-ui"},
-    )
-)
 
 
 @lru_cache
@@ -79,6 +54,71 @@ def get_shared():
     shared = {}
     Thread(target=bg_thread, args=[shared]).start()
     return shared
+
+
+app.layout = html.Div(
+    html.Div(
+        [
+            dcc.Markdown(
+                """
+# Who's interests are _nearest_ to mine?
+
+We calculate the distance matrix from the embeddings of your interests.   
+Then we plot a minimum spanning tree of the graph using a spring layout algorithm
+
+Hover to see the interests of the person.            
+                """
+            ),
+            dcc.Graph(
+                id="live-update-graph",
+                style={"height": "110vh"},
+            ),
+            dcc.Markdown(
+                """
+## Heatmap
+
+The distance matrix plotted as a heatmap.
+ 
+The darker the cell, the closer your interests are to them.   
+Note that this doesn't mean that the converse is also true. They might have better matches than you. 
+                """
+            ),
+            dcc.Graph(
+                id="live-update-heatmap",
+                style={"height": "100vh"},
+            ),
+            dcc.Markdown(
+                """
+## All submissions
+The list of all submissions.
+Note that if someone is in your nearest list, that doesn't mean you are in *their* nearest.                 
+                """
+            ),
+            dash_table.DataTable(
+                id="live-update-submissions",
+                fill_width=True,
+                style_table={
+                    "max-width": "100%",
+                    "overflow-x": "scroll",
+                },
+                style_header={
+                    "font-weight": "bold",
+                },
+                style_cell={
+                    "max-width": "100%",
+                    "padding": "5px",
+                    "text-align": "left",
+                },
+            ),
+            dcc.Interval(
+                id="interval-component",
+                interval=1000,  # in milliseconds
+                n_intervals=1,
+            ),
+        ],
+        style={"fontFamily": "system-ui"},
+    )
+)
 
 
 def bg_thread(shared):
@@ -92,69 +132,105 @@ def bg_thread(shared):
                 _on_change(df, shared)
             except:
                 print_exc()
-        sleep(1)
+        sleep(5)
 
 
-def _on_change(df, shared):
-    df = df.rename(
-        columns=lambda col: col.strip().lower().replace(" ", "_")
+def _on_change(df: pd.DataFrame, shared):
+    df = df.dropna(axis=1).dropna(axis=0)
+    n = len(df)
+
+    names = df["Name"].tolist()
+    interests = df["Interests"].tolist()
+
+    embeds = np.array(
+        [
+            embed
+            for i in range(0, n, batch_size)
+            for embed in get_embeddings(interests[i : i + batch_size])
+        ]
     )
-
-    keys = df.name.dropna().tolist()
-    docs = df.interests.dropna().tolist()
-
-    embeds = np.array([
-        embed
-        for i in range(0, len(docs), batch_size)
-        for embed in get_embeddings(docs[i: i + batch_size])
-    ])
-
-    data = umap.UMAP(random_state=42, n_neighbors=len(keys) // 2).fit_transform(
-        embeds
-    )
-    data = StandardScaler().fit_transform(data)
-    shared["plot"] = go.Scatter(
-        x=[row[0] for row in data],
-        y=[row[1] for row in data],
-        text=keys,
-        customdata=docs,
-        hovertemplate="<b>%{text}</b><br>%{customdata}<extra></extra>",
-        mode="markers+text",
-        textposition="top center",
-    )
-
     sims = embeds @ embeds.T
-    sorted_sims = np.dstack(
-        np.unravel_index(np.argsort(sims, axis=None), sims.shape)
-    )[0][::-1]
-    visited = set()
-    matches = []
-    for x, y in sorted_sims:
-        if x == y:
-            continue
-        if x in visited or y in visited:
-            continue
-        visited.add(x)
-        visited.add(y)
-        matches.append(
-            {
-                "Name 1": keys[x],
-                "Name 2": keys[y],
-                "Similarity": round(sims[x, y], 4),
-            }
-        )
-    shared["top_matches"] = pd.DataFrame.from_records(matches)
+
+    interests = list(map(wrap_text, interests))
+
+    G = nx.Graph()
+    for idx, name in enumerate(names):
+        G.add_node(idx, label=name)
+    for x in range(n):
+        for y in range(n):
+            if x == y:
+                continue
+            G.add_edge(x, y, weight=1 - sims[x, y])
+    G = nx.minimum_spanning_tree(G)
+    pos = nx.spring_layout(G, seed=42)
+
+    fig = go.Figure(
+        data=go.Scatter(
+            x=[pos[k][0] for k in pos],
+            y=[pos[k][1] for k in pos],
+            mode="markers+text",
+            text=[G.nodes[k]["label"] for k in pos],
+            textposition="top center",
+            customdata=interests,
+            hovertemplate="""
+<b>%{text}</b>
+<br>
+%{customdata}
+<extra></extra>
+            """,
+            marker=dict(
+                color=[sum(sims[k]) for k in pos],
+                colorscale="Greens",
+                size=10,
+            ),
+        ),
+    )
+    fig["layout"]["uirevision"] = "some-constant"
+    shared["fig"] = fig
+
+    heatmap = sims.copy()
+    heatmap[np.diag_indices_from(heatmap)] = np.nan
+    fig = go.Figure(
+        data=[
+            go.Heatmap(
+                z=heatmap,
+                x=names,
+                y=names,
+                customdata=[
+                    [f"{interests[x]}<br><br>{interests[y]}" for x in range(n)]
+                    for y in range(n)
+                ],
+                hovertemplate="""
+%{x} & %{y} (%{z:.4f})
+<br><br>
+%{customdata}
+<extra></extra>
+                """,
+                colorscale="YlGn",
+            )
+        ],
+        layout=go.Layout(
+            plot_bgcolor="rgba(0,0,0,0)",
+        ),
+    )
+    fig["layout"]["uirevision"] = "some-constant-heatmap"
+    shared["heatmap"] = fig
+
+    sims[np.diag_indices_from(sims)] = 0
+    top_k = np.argsort(sims, axis=1)[:, -3:]
+    nearests = [
+        [f"{names[y]} ({sims[x, y]:.3f})" for x, y in enumerate(top_k[:, k])]
+        for k in range(top_k.shape[1])
+    ]
+    pos = len(df.columns) - 1
+    for k in range(len(nearests)):
+        tag = string.ascii_uppercase[len(nearests) - k - 1]
+        df.insert(pos, f"Nearest {tag}", nearests[k])
+
+    shared["submissions"] = df.iloc[::-1]
 
 
 whitespace_re = re.compile("\s+")
-
-
-def get_embeddings(texts: list[str], engine: str = "text-embedding-ada-002"):
-    texts = [whitespace_re.sub(" ", text) for text in texts]
-    res = _openai_embedding_create(input=tuple(texts), engine=engine)
-    return [record["embedding"] for record in res["data"]]
-
-
 
 
 def download_sheet(f: furl, mime_type: str = "text/csv") -> bytes:
@@ -190,29 +266,42 @@ def url_to_gdrive_file_id(f: furl) -> str:
 
 
 @app.callback(
-    Output("live-update-graph", "figure"), Input("interval-component", "n_intervals"),
+    Output("live-update-graph", "figure"),
+    Input("interval-component", "n_intervals"),
 )
 def update_graph_live(n):
-    fig = make_subplots()
     data = get_shared()
-    if "plot" in data:
-        fig.add_trace(data["plot"])
-    return fig
+    if "fig" in data:
+        return data["fig"]
+    else:
+        return go.Figure()
 
 
 @app.callback(
-    Output("live-update-matches", "data"),
+    Output("live-update-heatmap", "figure"),
     Input("interval-component", "n_intervals"),
 )
-def update_matches(n):
+def update_heatmap_live(n):
     data = get_shared()
-    if "top_matches" not in data:
-        return
-    df = data["top_matches"]
-    return df.to_dict("records")
+    if "heatmap" in data:
+        return data["heatmap"]
+    else:
+        return go.Figure()
+
+
+@app.callback(
+    Output("live-update-submissions", "data"),
+    Input("interval-component", "n_intervals"),
+)
+def update_submisions(n):
+    shared = get_shared()
+    if "submissions" in shared:
+        return shared["submissions"].to_dict("records")
 
 
 F = typing.TypeVar("F", bound=typing.Callable[..., typing.Any])
+
+
 def redis_cache_decorator(fn: F) -> F:
     @wraps(fn)
     def wrapper(*args, **kwargs):
@@ -234,23 +323,35 @@ def redis_cache_decorator(fn: F) -> F:
     return wrapper
 
 
+def get_embeddings(texts: list[str]):
+    texts = [whitespace_re.sub(" ", text) for text in texts]
+    return _sentence_embedding_create(input=tuple(texts))
+
+
+# @redis_cache_decorator
+# def _openai_embedding_create(input, engine: str = "text-embedding-ada-002"):
+#     res = openai.Embedding.create(input=input, engine=engine)
+#     return [record["embedding"] for record in res["data"]]
+
+
 @redis_cache_decorator
-def _openai_embedding_create(*args, **kwargs):
-    return openai.Embedding.create(*args, **kwargs)
+def _sentence_embedding_create(input, engine="sentence-transformers/all-MiniLM-L6-v2"):
+    model = SentenceTransformer(engine)
+    embeddings = model.encode(input)
+    return embeddings
 
 
-# @app.callback(
-#     Output("live-update-text", "children"), Input("interval-component", "n_intervals")
-# )
-# def update_metrics(n):
-#     # lon, lat, alt = satellite.get_lonlatalt(datetime.datetime.now())
-#     lon, lat, alt = random.random() * 360, random.random() * 180, random.random() * 1000
-#     style = {"padding": "5px", "fontSize": "16px"}
-#     return [
-#         html.DataTa,
-#         html.Span("Latitude: {0:.2f}".format(lat), style=style),
-#         html.Span("Altitude: {0:0.2f}".format(alt), style=style),
-#     ]
+def wrap_text(text: str, maxlen: int = 80, sep: str = " …") -> str:
+    return "<br>".join(textwrap.wrap(text.strip(), width=maxlen))
+    # if len(text) <= maxlen:
+    #     return text
+    # assert len(sep) <= maxlen
+    # match = re.match(r"^(.{0,%d}\S)(\s)" % (maxlen - len(sep) - 1), text, flags=re.S)
+    # if match:
+    #     trunc = match.group(1)
+    # else:
+    #     trunc = text[: maxlen - len(sep)]
+    # return trunc + sep
 
 
 if __name__ == "__main__":
